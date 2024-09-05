4400a6304e08b69bbd5cd3cf6d635c365f1dea14
3phraimY <ephraim.young@cuw.edu>
2024-09-05 13:19:18 -0500

    comments added

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@ using namespace std;
 int main() {
 
     int size, height, width;
-    cout << "How many points would you like to plot in the matrix? >";
+    cout << "How many points would you like to plot in the matrix? > ";
     cin >> size;
-    cout << "What is the height of the matrix? >";
+    cout << "What is the height of the matrix? > ";
     cin >> height;
     if(height <= 0) {
         cout << "Error: Cannot have 0 or negative height";
         return 1;
     }
-    cout << "What is the width of the matrix? >";
+    cout << "What is the width of the matrix? > ";
     cin >> width;
     if(width <= 0) {
         cout << "Error: Cannot have 0 or negative width";
@@ -26,21 +26,21 @@ int main() {
 
     // prompt user for location and values of points
     for(int i = 0; i < size; i++) { 
-        cout << "Enter row position for point " << i + 1 << " >";
+        cout << "Enter row position for point " << i + 1 << " > ";
         cin >> elementTriplesArray[i].row;
         if(elementTriplesArray[i].row > height || elementTriplesArray[i].row < 0) {
             cout << "Error: Row out of defined matrix";
             return 1;
         }
 
-        cout << "Enter column position for point " << i + 1 << " >";
+        cout << "Enter column position for point " << i + 1 << " > ";
         cin >> elementTriplesArray[i].column;
         if(elementTriplesArray[i].column > width || elementTriplesArray[i].column < 0) {
             cout << "Error: Column out of defined matrix";
             return 1;
         }
 
-        cout << "Enter value for point " << i + 1 << " >";
+        cout << "Enter value for point " << i << " > ";
         cin >> elementTriplesArray[i].value;
         if(elementTriplesArray[i].value <= 0) {
             cout << "Error: Value cannot be 0 or negative";
@@ -49,11 +49,11 @@ int main() {
     }
     
     // create new sparse matrix given the provided data.
-    sparseMatrix* hi = new sparseMatrix(elementTriplesArray, size, width, height);
+    sparseMatrix* userDefinedMatrix = new sparseMatrix(elementTriplesArray, size, width, height);
 
     cout << "================================================" << endl;
-    hi->printMatrix();
+    userDefinedMatrix->printMatrix();
     cout << "================================================" << endl;
-    hi->printElementTriples();
+    userDefinedMatrix->printElementTriples();
     return 0;
 }
